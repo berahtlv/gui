a4f6462b2f70ddf8ca0d1a34f401901cd72589d2
berahtlv <robertsmg@gmail.com>
2018-06-15 22:36:40 +0300

    basic app layout

--- app/main.py
+++ app/main.py
@@ -3,21 +3,53 @@ import kivy
 kivy.require('1.10.0')
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 
 
 '''
 Main application Window
 '''
-class MainWindow(Widget):
+class MainWindow(BoxLayout):
     pass
 
+'''
+Main application menu
+'''
+class Menubar(BoxLayout):
+    pass
 
+'''
+Main toolbar for Menubar function shortcuts
+'''
+class Toolbar(BoxLayout):
+    pass
+
+'''
+Sidebar with drag and drop element list
+'''
 class Sidebar(BoxLayout):
     pass
 
+'''
+Sidebar element
+'''
+class SidebarIcon(Button):
+    pass
+
+'''
+Topology map with draggable elements
+'''
+class TopologyMap(FloatLayout):
+    pass
+
+'''
+Displays some mouseover info and application states
+'''
+class Statusbar(BoxLayout):
+    pass
 
 
 
