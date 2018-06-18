48aa29cceac0312fd7e6804286dbf848e28fba93
berahtlv <robertsmg@gmail.com>
2018-06-18 22:47:00 +0300

    sidebar icons and topomap property change

--- app/main.py
+++ app/main.py
@@ -1,19 +1,37 @@
 
 import kivy
-kivy.require('1.10.0')
+kivy.require('1.11.0')
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.behaviors.togglebutton import ToggleButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
 
+
+# icons location
+THEMES_FOLDER = 'themes/default/'
+# sidebar element list
+ELEMENT_LIST = [('roadm_on.jpg', 'roadm_off.jpg', Label(text='ROADM')),
+                ('amplifier_on.jpeg', 'amplifier_off.jpeg', Label(text='Amp')),
+                ('transmitter_on.jpg', 'transmitter_off.jpg', Label(text='Trx')),
+                ('fiber_on.jpg', 'fiber_off.jpg', Label(text='Fiber'))]
 
 '''
 Main application Window
 '''
 class MainWindow(BoxLayout):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # workaround to get all MainWindow ids
+        global root
+        root = self
 
 '''
 Main application menu
@@ -21,37 +39,66 @@ Main application menu
 class Menubar(BoxLayout):
     pass
 
+
 '''
 Main toolbar for Menubar function shortcuts
 '''
 class Toolbar(BoxLayout):
     pass
 
-'''
-Sidebar with drag and drop element list
-'''
-class Sidebar(BoxLayout):
-    pass
 
 '''
-Sidebar element
+Sidebar with element list
 '''
-class SidebarIcon(Button):
-    pass
+class Sidebar(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # TODO: remove lazy workaround for icons centering
+        self.add_widget(Widget())
+        for el in ELEMENT_LIST:
+            self.add_widget(SidebarIcon(el))
+        self.add_widget(Widget())
+
+
+'''
+Sidebar element object
+images = (image_on, image_off, obj)
+'''
+class SidebarIcon(ToggleButtonBehavior, Image):
+
+    def __init__(self, images, **kwargs):
+        super().__init__(**kwargs)
+        self.img_on, self.img_off, self.el_type = images
+        self.source = THEMES_FOLDER + self.img_off
+
+    def on_state(self, widget, value):
+        if value == 'down':
+            self.source = THEMES_FOLDER + self.img_on
+            root.ids['topo_map'].active_el = self.el_type
+            print(root.ids['topo_map'].active_el.text)
+        else:
+            self.source = THEMES_FOLDER + self.img_off
+            root.ids['topo_map'].active_el = None
+            print(root.ids['topo_map'].active_el)
+
 
 '''
 Topology map with draggable elements
 '''
 class TopologyMap(FloatLayout):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.active_el = ObjectProperty(None, allownone=True)
+
 
 '''
 Displays some mouseover info and application states
 '''
 class Statusbar(BoxLayout):
     pass
-
-
 
 
 '''
@@ -62,8 +109,8 @@ class Application(App):
     # TODO:
     # 1. default app config and its location
     # 1.1. main window size restriction
-    # 1.2. icons theme location
-    # 1.3. default working directory
+    # 1.2. icons theme location (THEMES_FOLDER)
+    # 1.3. default working directory (PROJECTS_FOLDER)
 
     def build(self):
         mainwindow = Builder.load_file('./kv/main.kv')
